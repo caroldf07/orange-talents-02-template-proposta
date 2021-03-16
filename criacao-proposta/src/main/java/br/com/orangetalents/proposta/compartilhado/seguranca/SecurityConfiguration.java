0175d2e7466b8288c3b7cd7411d99c5f3cce8486
caroldf07 <carolina.df@outlook.com>
2021-03-16 19:09:20 -0300

    Micrometer e Prometheus configurados

--- criacao-proposta/src/main/java/br/com/orangetalents/proposta/compartilhado/seguranca/SecurityConfiguration.java
+++ criacao-proposta/src/main/java/br/com/orangetalents/proposta/compartilhado/seguranca/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.orangetalents.proposta.compartilhado.seguranca;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +16,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests().antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
